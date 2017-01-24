0fdc2a9ecf0bc4b322e31a3155a26cf73782c5ef
hamakazeTorobe <t.hamakaze0909@gmail.com>
2017-01-25 02:26:28 +0900

    updated function

--- hashChecker.sh
+++ hashChecker.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-echo "Hello, World!"
+if [ $# -ne 2 ]; then
+    echo "[error] please select 2 files" 1>&2
+    exit 1
+fi
+
+echo "hash check start..."
+hash1=`md5sum $1`
+hash2=`md5sum $2`
+
+hash1=`echo ${hash1} | sed -e 's/\s.*//'`
+hash2=`echo ${hash2} | sed -e 's/\s.*//'`
+#echo ${hash1}
+#echo ${hash2}
+if [ ${hash1} = ${hash2} ]; then
+    echo "found plagiarist!"
+    exit 0
+fi
+
+echo "different file"
+exit 0
